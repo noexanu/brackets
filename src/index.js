7fb47cbf401c86c06a282d64cf181f7d628ac549
noexanu <70746953+noexanu@users.noreply.github.com>
2020-10-19 22:27:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let brackets = ''
+	for (subArr of bracketsConfig) {
+		for (elem of subArr) {
+			brackets += elem
+		}
+	}
+	const antonym = (letter) => brackets.includes(letter) ? brackets[brackets.lastIndexOf(letter) - 1] : false
+	let previousBracket = ''
+	for (letter of str) {
+		if (previousBracket.slice(-1) === antonym(letter)) {
+			previousBracket = previousBracket.slice(0, -1)
+		} else {
+			previousBracket += letter
+		}
+	}
+	return previousBracket.length != 0 ? false : true
 }
